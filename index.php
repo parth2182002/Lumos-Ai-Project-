366dd59fdbcf9d2c35770cfe92ecdef352e2cc41
parth2182002 <161914755+parth2182002@users.noreply.github.com>
2026-04-22 10:29:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Electrical Ai | Control and Power Panel Solutions</title>
+    <meta name="author" content="Electrical Ai" />
+    <link rel="sitemap" type="application/xml" href="https://agent.infinityfree.me/sitemap.xml" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <script>
+      window.ELECTRICAL_AI_ILLUSTRATIONS = {
+        leftHand: "illustrations/68.png",
+        sea: "illustrations/905.png",
+        man: "illustrations/2785.png",
+        defaultPanel: "illustrations/538.png"
+      };
+    </script>
+    <script type="module" crossorigin src="index1.js"></script>
+    <link rel="stylesheet" crossorigin href="index.css">
+
+  </head>
+
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
